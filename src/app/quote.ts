073a29ab5d6543384c565a198c920e809b4e7678
Mugisha Thierry <thierrymugis@gmail.com>
2020-11-09 01:44:40 +0200

    Added quote component

---
+++ src/app/quote.ts
@@ -0,0 +1,17 @@
+export class Quote {
+    showDescription: boolean;
+
+    constructor(
+        public id: number, 
+        public quote: string, 
+        public author: string, 
+        public publisher: string, 
+        public completeDate: Date,
+        public like:number,
+        public dislike:number
+        )
+        {
+         this.showDescription=false;
+         
+    }
+}
