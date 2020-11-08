dff0cb34d5b46f68968a62fa80d416058dd21acd
Mugisha Thierry <thierrymugis@gmail.com>
2020-11-09 01:43:09 +0200

    Added a form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectFormComponent } from './project-form/project-form.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    ProjectFormComponent,
+    ProjectDetailsComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
